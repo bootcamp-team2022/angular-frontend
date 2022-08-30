2ce79b2b0722040366a7d1c0ffea8d70688627fc
Mario Smith <mariosmith.dev@gmail.com>
2022-08-30 16:59:49 -0400

    Reverted Angular App Changes

--- src/app/services/contact.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from  '@angular/common/http';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ContactService {
-  constructor(private  httpClient:  HttpClient) {}
-}
