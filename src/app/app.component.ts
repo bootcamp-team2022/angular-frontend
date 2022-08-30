923a722382053004cdb74a24d5e59d07517d0bf8
Mario Smith <mariosmith.dev@gmail.com>
2022-08-30 00:20:03 -0400

    Reverted Test File

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Welcome to the bootcamps!';
+  title = 'Welcome to the bootcamp!';
 }
