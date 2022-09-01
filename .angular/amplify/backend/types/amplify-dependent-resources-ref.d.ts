618b57ecfbc58e2a9b5d468a5e6feaea3fe1eede
Amber Mosby <amosby909@gmail.com>
2022-09-01 07:55:13 -0700

    BW69

---
+++ .angular/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "bootcamp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
